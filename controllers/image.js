a301caf0876d041235ba3edf917441dee1209ba4
kl <kl@debian>
2019-11-30 18:11:23 +0800

    Add feature to detect multiple faces

--- controllers/image.js
+++ controllers/image.js
@@ -9,6 +9,7 @@ const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
+      console.log(data);
       res.json(data);
     })
     .catch(err => res.status(400).json("unable to work with API"));
