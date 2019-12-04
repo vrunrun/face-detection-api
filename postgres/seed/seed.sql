e1f79588da0ac267c959e026a42e219f8b453dd7
kl <kl@debian>
2019-12-04 11:39:33 +0800

    Modified seed data

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 
 INSERT INTO users(name, email, entries, joined) 
-VALUES ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+VALUES ('a', 'a@a.com', 5, '2018-01-01');
 
 INSERT INTO login(hash, email) 
-VALUES ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'jessie@gmail.com');
+VALUES ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'a@a.com');
 
 COMMIT;
