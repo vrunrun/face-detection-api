5ee0a8627bd01e19ad83f5fe8e7f2307a938b59d
kl <kl@debian>
2019-12-08 14:24:16 +0800

    Created JWT token and be able to send it to the front-end client

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,60 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require("jsonwebtoken");
+
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject("incorrect form submission");
   }
-  db.select('email', 'hash').from('login')
-    .where('email', '=', email)
+  return db
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then(user => user[0])
+          .catch(err => Promise.reject("unable to get user"));
       } else {
-        res.status(400).json('wrong credentials')
+        Promise.reject("wrong credentials");
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
-}
+    .catch(err => Promise.reject("wrong credentials"));
+};
+
+const getAuthTokenId = () => {
+  console.log("auth ok");
+};
+
+const signToken = email => {
+  const jwtPayload = { email };
+  return jwt.sign({ jwtPayload }, "JWT_SECRET", { expiresIn: "2 days" });
+};
+
+const createSessions = user => {
+  //JWT token, return user data
+  const { email, id } = user;
+  const token = signToken(email, id);
+  return { success: "true", userId: id, token: token };
+};
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization
+    ? getAuthTokenId()
+    : handleSignin(db, bcrypt, req, res)
+        .then(data => {
+          return data.id && data.email
+            ? createSessions(data)
+            : Promise.reject(data);
+        })
+        .then(session => res.json(session))
+        .catch(err => res.status(400).json(err));
+};
 
 module.exports = {
-  handleSignin: handleSignin
-}
+  signinAuthentication: signinAuthentication
+};
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
   // res.send("It's working");
 });
 
-app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
