b8e6b55dc22b2a268758cfc2d296645735529711
kl <kl@debian>
2019-12-09 13:22:17 +0800

    Add middleware to validate user before accessing endpoints

--- controllers/signin.js
+++ controllers/signin.js
@@ -74,5 +74,6 @@ const signinAuthentication = (db, bcrypt) => (req, res) => {
 };
 
 module.exports = {
-  signinAuthentication: signinAuthentication
+  signinAuthentication: signinAuthentication,
+  redisClient: redisClient
 };
